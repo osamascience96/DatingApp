84644a10d2ffdcfc5f1ff3c6f6e8b4fb14ec802d
osamascience96 <osamahu96@gmail.com>
2023-12-02 14:11:54 +0500

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(cors => cors.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
